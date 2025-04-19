0745b02459ff4832b259c5922ba345d3f7e07fcb
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-04-20 01:13:37 +0200

    refactor App class to integrate OrderService and streamline scene routing

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,24 +4,22 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import javafx.application.Application;
-import javafx.geometry.Insets;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
-import org.example.router.SceneRouter;
+import org.example.shared.AppControllerFactory;
+import org.example.features.order.OrderService;
+import org.example.shared.SceneRouter;
 
 public class App extends Application {
+
+  private final OrderService orderService = new OrderService();
   
   @Override
   public void start(Stage primaryStage) {
-    VBox root = new VBox();
-    root.setPadding(new Insets(5));
-    Label title = new Label("JavaFX");
-    Button goToHomeButton = new Button("Go to Home");
 
-    goToHomeButton.setOnAction(event -> SceneRouter.goToHomePage());
+
+    SceneRouter router = new SceneRouter(primaryStage, orderService);
+    AppControllerFactory controllerFactory = new AppControllerFactory(orderService, router);
+    router.setControllerFactory(controllerFactory);
 
     try {
       Connection conn = DriverManager.getConnection(
@@ -32,17 +30,8 @@ public class App extends Application {
       System.err.println("Connection failed: " + e.getMessage());
     }
 
-    root.getChildren().addAll(title);
-
-    SceneRouter.setStage(primaryStage);
-    SceneRouter.goTo(SceneRouter.KioskPage.HOME);
-
+    router.goTo(SceneRouter.KioskPage.HOME);
     primaryStage.setTitle("JavaFX with MySQL");
-    root.getChildren().add(goToHomeButton);
-    Scene scene = new Scene(root, 300, 250);
-
-    primaryStage.setScene(scene);
-
     primaryStage.show();
 
   }
