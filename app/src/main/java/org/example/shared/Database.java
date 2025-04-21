fc357875ba6c51183d1690873beb2044313c75d9
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-04-21 22:23:43 +0200

    feat: add Database singleton class for MySQL connection management

---
+++ app/src/main/java/org/example/shared/Database.java
@@ -0,0 +1,42 @@
+package org.example.shared;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+public class Database {
+
+  private static Database instance;
+  private Connection connection;
+
+  private final String URL = "jdbc:mysql://localhost/kioske?useSSL=false&allowPublicKeyRetrieval=true";
+  private final String USER = "main";
+  private final String PASSWORD = "root";
+
+  private Database() {
+    try {
+      this.connection = DriverManager.getConnection(URL + "&user=" + USER + "&password=" + PASSWORD);
+    } catch (SQLException e) {
+      System.err.println("Connection failed: " + e.getMessage());
+    }
+  }
+
+  public static synchronized Database getInstance() {
+    if (instance == null) {
+      instance = new Database();
+    }
+    return instance;
+  }
+
+  public Connection getConnection() {
+    try {
+      if (connection == null || connection.isClosed()) {
+        connection = DriverManager.getConnection(URL + "&user=" + USER + "&password=" + PASSWORD);
+      }
+    } catch (SQLException e) {
+      System.err.println("Reconnection failed: " + e.getMessage());
+    }
+    return connection;
+  }
+}
