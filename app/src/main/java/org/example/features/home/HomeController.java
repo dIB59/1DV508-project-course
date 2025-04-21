d9a338e26c4e23ba56c97aa9c9d154359fcf9a4f
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-04-22 00:01:13 +0200

    feat: add documentation comments to HomeController class and its methods

--- app/src/main/java/org/example/features/home/HomeController.java
+++ app/src/main/java/org/example/features/home/HomeController.java
@@ -5,10 +5,16 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import org.example.shared.SceneRouter;
 
+/**
+ * The type Home controller.
+ */
 public class HomeController {
 
   private final SceneRouter sceneRouter;
   private final HomeModel homeModel;
+  /**
+   * The Item count label.
+   */
   @FXML
   public Label itemCountLabel;
   @FXML
@@ -17,11 +23,20 @@ public class HomeController {
   private Button checkoutButton;
 
 
+  /**
+   * Instantiates a new Home controller.
+   *
+   * @param homeModel   the home model
+   * @param sceneRouter the scene router
+   */
   public HomeController(HomeModel homeModel, SceneRouter sceneRouter) {
     this.homeModel = homeModel;
     this.sceneRouter = sceneRouter;
   }
 
+  /**
+   * Got to menu page.
+   */
   @FXML
   public void gotToMenuPage() {
     sceneRouter.goToMenuPage();
