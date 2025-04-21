7249a72d3ef8d54d1a9bd30446cd3e291ea15677
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-04-22 00:03:15 +0200

    feat: add documentation comments to MenuModel class and its constructor

--- app/src/main/java/org/example/features/menu/MenuModel.java
+++ app/src/main/java/org/example/features/menu/MenuModel.java
@@ -1,7 +1,13 @@
 package org.example.features.menu;
 
+/**
+ * The type Menu model.
+ */
 public class MenuModel {
 
+  /**
+   * Instantiates a new Menu model.
+   */
   public MenuModel() {
 
   }
