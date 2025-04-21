84d97edf958e15027527be2b5ef051a2d9c3b860
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-04-21 23:59:45 +0200

    feat: update order repository declaration to use CrudRepository interface

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,9 +4,11 @@ package org.example;
 import java.sql.Connection;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import org.example.features.order.Order;
 import org.example.features.order.OrderMapper;
 import org.example.features.order.OrderRepository;
 import org.example.features.order.OrderService;
+import org.example.shared.CrudRepository;
 import org.example.shared.Database;
 import org.example.shared.SceneRouter;
 
@@ -19,7 +21,7 @@ public class App extends Application {
     Connection conn = Database.getInstance().getConnection();
 
     var orderMapper = new OrderMapper();
-    var orderRepository = new OrderRepository(conn, orderMapper);
+    CrudRepository<Order> orderRepository = new OrderRepository(conn, orderMapper);
     var orderService = new OrderService(orderRepository);
 
     SceneRouter router = new SceneRouter(primaryStage, orderService);
