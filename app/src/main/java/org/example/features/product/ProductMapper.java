2bea2435b6dd0fa84bea588ea78d27dd7351f2b3
Ibrahim Iqbal <ibrahim@ebs.net.pk>
2025-04-21 22:30:49 +0200

    feat: add ProductMapper to map ResultSet to Product entity

---
+++ app/src/main/java/org/example/features/product/ProductMapper.java
@@ -0,0 +1,18 @@
+package org.example.features.product;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.example.shared.EntityMapper;
+
+public class ProductMapper implements EntityMapper<Product> {
+
+  @Override
+  public Product map(ResultSet rs) throws SQLException {
+    return new Product(
+        rs.getInt("id"),
+        rs.getString("name"),
+        rs.getString("description"),
+        rs.getDouble("price"),
+        rs.getString("image_url"));
+  }
+}
