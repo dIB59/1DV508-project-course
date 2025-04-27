7cc09585a838d39fa35ff65233e6296e2f4ebac2
Ibrahim Iqbal <si222my@student.lnu.se>
2025-04-27 21:51:22 +0200

    feat: extend Product record to include tags and related methods

--- app/src/main/java/org/example/features/product/ProductMapper.java
+++ app/src/main/java/org/example/features/product/ProductMapper.java
@@ -2,6 +2,9 @@ package org.example.features.product;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import org.example.database.EntityMapper;
 
 /** The type Product mapper. */
@@ -9,11 +12,35 @@ public class ProductMapper implements EntityMapper<Product> {
 
   @Override
   public Product map(ResultSet rs) throws SQLException {
+    String tagsString = rs.getString("tags");
+    List<String> tagsName = new ArrayList<>();
+    if (tagsString != null && !tagsString.isEmpty()) {
+      tagsName.addAll(Arrays.asList(tagsString.split(",")));
+    }
+    String tagIdsString = rs.getString("tags_ids");
+    List<Integer> tagIds = new ArrayList<>();
+    if (tagIdsString != null && !tagIdsString.isEmpty()) {
+      for (String tagId : tagIdsString.split(",")) {
+        try {
+          tagIds.add(Integer.parseInt(tagId));
+        } catch (NumberFormatException e) {
+          throw new RuntimeException("Shit went wrong");
+        }
+      }
+    }
+
+    List<Tag> tags = new ArrayList<>();
+    for (int i = 0; i < tagsName.size(); i++) {
+      tags.add(new Tag(tagIds.get(i), tagsName.get(i)));
+    }
+
     return new Product(
         rs.getInt("id"),
         rs.getString("name"),
         rs.getString("description"),
         rs.getDouble("price"),
-        rs.getString("image_url"));
+        rs.getString("image_url"),
+        tags
+        );
   }
 }
