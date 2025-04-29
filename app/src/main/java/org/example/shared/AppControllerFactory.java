68bb009c89de006eca79985e96b023c98c92c71c
Ibrahim Iqbal <si222my@student.lnu.se>
2025-04-29 21:10:33 +0200

    feat: update AppControllerFactory to include CouponsRepository and optimize database connection usage

--- app/src/main/java/org/example/shared/AppControllerFactory.java
+++ app/src/main/java/org/example/shared/AppControllerFactory.java
@@ -1,12 +1,14 @@
 package org.example.shared;
 
 import java.lang.reflect.InvocationTargetException;
+import java.sql.Connection;
 import javafx.util.Callback;
 import org.example.database.Database;
 import org.example.features.admin.AdminController;
 import org.example.features.admin.AdminMapper;
 import org.example.features.admin.AdminRepository;
 import org.example.features.checkout.CheckoutController;
+import org.example.features.coupons.CouponsRepository;
 import org.example.features.dashboard.DashboardController;
 import org.example.features.dashboard.DashboardModel;
 import org.example.features.home.HomeController;
@@ -28,6 +30,7 @@ public class AppControllerFactory implements Callback<Class<?>, Object> {
 
   private final OrderService orderService;
   private final SceneRouter sceneRouter;
+  private final Connection connection = Database.getInstance().getConnection();
 
   /**
    * Instantiates a new App controller factory.
@@ -53,7 +56,7 @@ public class AppControllerFactory implements Callback<Class<?>, Object> {
       case "HomeController" -> new HomeController(new HomeModel(), sceneRouter);
       case "MenuController" ->
           new MenuController(new MenuModel(), getProductRepository(), sceneRouter, orderService);
-      case "CheckoutController" -> new CheckoutController(orderService, sceneRouter);
+      case "CheckoutController" -> new CheckoutController(orderService, getCouponsRepository(), sceneRouter);
       case "ProductDetailsController" -> new ProductDetailsController(orderService, sceneRouter);
       case "ReceiptController" ->
           new ReceiptController(orderService.saveOrderAndClear(), sceneRouter);
@@ -75,10 +78,14 @@ public class AppControllerFactory implements Callback<Class<?>, Object> {
   }
 
   private ProductRepository getProductRepository() {
-    return new ProductRepository(Database.getInstance().getConnection(), new ProductMapper());
+    return new ProductRepository(connection, new ProductMapper());
   }
 
   private AdminRepository getAdminRepository() {
-    return new AdminRepository(Database.getInstance().getConnection(), new AdminMapper());
+    return new AdminRepository(connection, new AdminMapper());
   }
+
+  private CouponsRepository getCouponsRepository() {
+    return new CouponsRepository(connection);
+    }
 }
