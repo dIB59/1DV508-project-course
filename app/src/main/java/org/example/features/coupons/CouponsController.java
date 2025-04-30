ed8502a886eb78be960d490722ca6e61f148d274
Santiago Jhosep Peña Hallasi <sp223pv@student.lnu.se>
2025-04-30 16:40:29 +0200

    Add handleAddCouponButtonAction method to CouponsController for coupon creation

--- app/src/main/java/org/example/features/coupons/CouponsController.java
+++ app/src/main/java/org/example/features/coupons/CouponsController.java
@@ -1,13 +1,41 @@
 package org.example.features.coupons;
 
+import java.sql.SQLException;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 import org.example.shared.SceneRouter;
 
 public class CouponsController {
   private final CouponsRepository couponsRepository;
   private final SceneRouter sceneRouter;
+  @FXML private TextField codeField;
+  @FXML private TextField discountField;
 
   public CouponsController(CouponsRepository couponsRepository, SceneRouter sceneRouter) {
     this.couponsRepository = couponsRepository;
     this.sceneRouter = sceneRouter;
+  }
+
+  /**
+   * Go to dashboard page.
+   *
+   * <p>This method is called when the user clicks the "Go to Dashboard" button.
+   */
+  public void handleAddCouponButtonAction() {
+    String code = codeField.getText();
+    String discount = discountField.getText();
+
+    if (code.isEmpty() || discount.isEmpty()) {
+      codeField.setText("Code or Discount cannot be empty");
+      discountField.setText("");
+      return;
+    }
+
+    try {
+      couponsRepository.save(new Coupons(code, Double.parseDouble(discount)* 100));
+    } catch (SQLException | NumberFormatException e) {
+      throw new RuntimeException(e);
+    }
+    sceneRouter.goToDashboardPage();
   }
 }
