f8ede1674f156592acae9fc9788360674dbe711c
Santiago Jhosep Peña Hallasi <sp223pv@student.lnu.se>
2025-05-01 09:53:36 +0200

    Add delete coupon functionality to CouponsController

--- app/src/main/java/org/example/features/coupons/CouponsController.java
+++ app/src/main/java/org/example/features/coupons/CouponsController.java
@@ -1,8 +1,14 @@
 package org.example.features.coupons;
 
 import java.sql.SQLException;
+import java.util.List;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import org.example.features.product.Product;
 import org.example.shared.SceneRouter;
 
 public class CouponsController {
@@ -10,12 +16,29 @@ public class CouponsController {
   private final SceneRouter sceneRouter;
   @FXML private TextField codeField;
   @FXML private TextField discountField;
+  @FXML private VBox couponsList;
 
   public CouponsController(CouponsRepository couponsRepository, SceneRouter sceneRouter) {
     this.couponsRepository = couponsRepository;
     this.sceneRouter = sceneRouter;
   }
 
+  /** Initialize. */
+  public void initialize() {
+    for (Coupons coupons : getCouponsList()) {
+      Button addButton = new Button("Delete coupon");
+
+      Label code = new Label(coupons.getCode());
+      Label discount = new Label(String.format("$%.2f", coupons.getDiscount()));
+
+      VBox coupon = new VBox(code, discount, addButton);
+      coupon.setSpacing(5);
+      coupon.setStyle("-fx-padding: 10; -fx-border-color: #ccc; -fx-border-radius: 5;");
+
+      // addButton.setOnAction(event -> orderService.addItem(product));
+      couponsList.getChildren().add(coupon);
+    }
+  }
   /**
    * Go to dashboard page.
    *
@@ -36,6 +59,35 @@ public class CouponsController {
     } catch (SQLException | NumberFormatException e) {
       throw new RuntimeException(e);
     }
+    sceneRouter.goToDashboardPage();
+  }
+
+  public void handleDeleteCouponButtonAction() {
+    String code = codeField.getText();
+    if (code.isEmpty()) {
+      codeField.setText("Code cannot be empty");
+      return;
+    }
+
+    try {
+      couponsRepository.delete(code);
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    sceneRouter.goToDashboardPage();
+  }
+
+  public List<Coupons> getCouponsList() {
+    List<Coupons> coupons;
+    try {
+      coupons = couponsRepository.findAll();
+    } catch (SQLException e) {
+      throw new RuntimeException(e);
+    }
+    return coupons;
+  }
+
+  public void goToDashboardPage(ActionEvent event) {
     sceneRouter.goToDashboardPage();
   }
 }
