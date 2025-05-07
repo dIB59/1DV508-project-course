deccd90ba4eef99c468f242413100af61bf038c3
Abhinav Mudugu <am225mq@student.lnu.se>
2025-05-07 12:46:29 +0200

    changed app config to generate random keys if .env not found

--- app/src/main/java/org/example/AppConfig.java
+++ app/src/main/java/org/example/AppConfig.java
@@ -3,17 +3,17 @@ package org.example;
 import io.github.cdimascio.dotenv.Dotenv;
 
 public class AppConfig {
-  private static final Dotenv dotenv = Dotenv.load();
+  private static final Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
 
   public static String getMerchantId() {
-    return dotenv.get("MERCHANT_ID");
+    return dotenv.get("MERCHANT_ID", "defaultMerchantId");
   }
 
   public static String getPayPalPrivateKey() {
-    return dotenv.get("PAYPAL_PRIVATE_ID");
+    return dotenv.get("PAYPAL_PRIVATE_ID", "defaultPrivateKey");
   }
 
   public static String getPayPalPublicKey() {
-    return dotenv.get("PAYPAL_PUBLIC_KEY");
+    return dotenv.get("PAYPAL_PUBLIC_KEY", "defaultPublicKey");
   }
 }
