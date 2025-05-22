b2cb4c530c052c7690389b2549f744187dbf47dd
Ibrahim Iqbal <si222my@student.lnu.se>
2025-05-22 20:10:47 +0200

    refactor: remove HomeModel dependency from HomeController and simplify its constructor

--- app/src/main/java/org/example/features/feedback/FeedbackController.java
+++ app/src/main/java/org/example/features/feedback/FeedbackController.java
@@ -2,16 +2,13 @@ package org.example.features.feedback;
 
 import javafx.scene.control.Button;
 import org.example.features.order.OrderService;
-import java.sql.SQLException;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import org.example.shared.SceneRouter;
 
 public class FeedbackController {
   private final SceneRouter sceneRouter;
-  private OrderService orderService;
+  private final OrderService orderService;
 
   public  FeedbackController(SceneRouter sceneRouter, OrderService orderService) {
     this.sceneRouter = sceneRouter;
@@ -28,7 +25,7 @@ public class FeedbackController {
     Button clickedButton = (Button) event.getSource();
 
     // Get parent HBox and find index of clicked button
-    int rating = ((javafx.scene.layout.HBox) clickedButton.getParent())
+    int rating = clickedButton.getParent()
         .getChildrenUnmodifiable()
         .indexOf(clickedButton) + 1;
 
